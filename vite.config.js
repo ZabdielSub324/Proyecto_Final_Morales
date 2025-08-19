e23ee81d6b9141380537f18d5b68ded05d35d878
ZabdielSub324 <zabdiel_3041220173@utd.edu.mx>
2025-08-19 12:59:16 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/ZabdielSub324/Proyecto_Final_Morales.git',
+  base: '/Proyecto_Final_Morales/',
 })
